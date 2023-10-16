a303a7614382543dad772b8610dc1123279a7aea
samfran <siamack2pazjeta@gmail.com>
2023-10-16 00:36:50 -0700

    create Movie model

---
+++ src/main/kotlin/com/cinema/models/Movie.kt
@@ -0,0 +1,9 @@
+package com.cinema.models
+
+data class Movie(
+    private val id: Int,
+    private val title: String,
+    private val description: String,
+    private val duration: Int,
+    private val year: Int
+)
